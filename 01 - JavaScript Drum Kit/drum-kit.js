ae5b660d8a8b1481c0827eb69b391e80e0095649
Mark Bailey <markbailey0356@gmail.com>
2018-09-05 00:57:58 +0930

    Implement drum release on transition end

--- 01 - JavaScript Drum Kit/drum-kit.js
+++ 01 - JavaScript Drum Kit/drum-kit.js
@@ -2,7 +2,13 @@ const aBtn = document.querySelector("#a");
 console.log(aBtn);
 
 aBtn.addEventListener("click", hitDrum);
+aBtn.addEventListener("transitionend", releaseDrum);
 
 function hitDrum() {
   console.log("The 'a' drum has been hit!");
+  aBtn.classList.add("playing");
+}
+
+function releaseDrum() {
+  aBtn.classList.remove("playing");
 }
