78c3f033a9b7d7000c186adc6fd46686f9da8c76
Mark Bailey <markbailey0356@gmail.com>
2018-09-05 01:31:39 +0930

    Restrict keypress to correct keyCode

--- 01 - JavaScript Drum Kit/drum-kit.js
+++ 01 - JavaScript Drum Kit/drum-kit.js
@@ -1,11 +1,21 @@
 const aBtn = document.querySelector("#a");
-console.log(aBtn);
 
-aBtn.addEventListener("click", hitDrum);
+window.addEventListener("keydown", keyDrum);
+
+aBtn.addEventListener("click", clickDrum);
 aBtn.addEventListener("transitionend", releaseDrum);
 
+function keyDrum(e) {
+  if (e.keyCode == 65) {
+    hitDrum();
+  }
+}
+
+function clickDrum(e) {
+  hitDrum();
+}
+
 function hitDrum() {
-  console.log("The 'a' drum has been hit!");
   aBtn.classList.add("playing");
   var keyCode = aBtn.getAttribute("data-key");
   var sound = document.querySelector('audio[data-key="'+keyCode+'"]');
